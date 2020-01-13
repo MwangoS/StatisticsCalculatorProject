7168840f9a576b493cfba562d9d92c97e5b5d481
clara9621 <58587888+clara9621@users.noreply.github.com>
2020-01-13 17:33:24 +0200

    Task: created Basic Calculator

---
+++ CalculatorBasic/BasicCalculator.cs
@@ -0,0 +1,25 @@
+﻿public class Division
+
+
+
+{
+
+
+    public static int Divide(int a, int b)
+
+    {
+
+        return a / b;
+
+    }
+
+    public static double Divide(double a, double b)
+
+    {
+
+        return a / b;
+
+
+
+    }
+}
---
+++ CalculatorBasic/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorBasic
+{
+    class Class1
+    {
+    }
+}
